9369ad29979ed87b4490a263782c9ab7c0c2aac6
Surya <git@sdan.io>
2024-02-28 00:53:46 -0800

    prod

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+from vlite2 import VLite2
+import json
+
+app = Flask(__name__)
+
+# Load the VLite2 database
+vdb = VLite2(vdb_name="ye_tweets")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.json['query']
+    results = vdb.retrieve(text=query, top_k=10, get_metadata=True)
+    return jsonify(results['texts'])
+
+if __name__ == '__main__':
+    app.run()
